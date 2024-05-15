f230a86b493154024ad79677006d06f087338299
EC2 Default User <ec2-user@ip-172-31-46-98.us-west-2.compute.internal>
2024-05-15 00:10:25 +0000

    Adding dockerfile, terraform for apigateway

--- main.tf
+++ main.tf
@@ -29,10 +29,6 @@ resource "aws_s3_bucket_object" "object" {
   key    = "todo-data.json"
   source = "todo-data.json"
 
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  # etag = filemd5("ptodo-data.json")
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -50,23 +46,8 @@ assume_role_policy = jsonencode({
       },
     ]
   })
+
 }
-#assume_role_policy = <<EOF
-#{
-# "Version": "2012-10-17",
-# "Statement": [
-#   {
-#     "Action": "sts:AssumeRole",
-#     "Principal": {
-#       "Service": "lambda.amazonaws.com"
-#     },
-#     "Effect": "Allow",
-#     "Sid": ""
-#   }
-# ]
-#}
-#EOF
-#}
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
  
@@ -113,33 +94,124 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
   source_dir  = "${path.module}/python/"
-  output_path = "${path.module}/python/todo-lambda.zip"
+  output_path = "${path.module}/python/lambda_function.zip"
 }
  
 resource "aws_lambda_function" "terraform_lambda_func" {
-  filename                       = "${path.module}/python/todo-lambda.zip"
+  filename                       = "${path.module}/python/lambda_function.zip"
   function_name                  = "c2-g4-tf-get-to-do"
   role                           = aws_iam_role.lambda_role.arn
-  handler                        = "todo.lambda_handler"
+  handler                        = "lambda_function.lambda_handler"
   runtime                        = "python3.10"
   depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
+resource "aws_api_gateway_rest_api" "my_api" {
 
-#Policies:
-#        - Statement:
-#            - Effect: Allow
-#              Action:
-#                - s3:GetObject
-#              Resource: arn:aws:s3:::*
-#            - Effect: Allow
-#              Action:
-#                - logs:CreateLogGroup
-#              Resource: arn:aws:logs:us-west-2:962804699607:*
-#            - Effect: Allow
-#              Action:
-#                - logs:CreateLogStream
-#                - logs:PutLogEvents
-#              Resource:
-#                - >-
-#                  arn:aws:logs:us-west-2:962804699607:log-group:/aws/lambda/c2-g4-get-to-do:*
+  name = "c2-g4-tf-todo-api"
+  description = "Lambda API gateway"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+  
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part = "get-todo"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "GET"
+  type = "AWS"
+  uri = aws_lambda_function.terraform_lambda_func.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  depends_on = [
+    aws_api_gateway_method.proxy,
+    aws_api_gateway_integration.lambda_integration
+  ]
+}
+
+resource "aws_api_gateway_method" "proxy_options" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration_options" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy_options.http_method
+  integration_http_method = "OPTIONS"
+  type = "MOCK"
+}
+
+resource "aws_api_gateway_method_response" "proxy_options" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy_options.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "proxy_options" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy_options.http_method
+  status_code = aws_api_gateway_method_response.proxy_options.status_code
+
+  depends_on = [
+    aws_api_gateway_method.proxy_options,
+    aws_api_gateway_integration.lambda_integration_options
+  ]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.lambda_integration_options, # Add this line
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name = "prod"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role = aws_iam_role.lambda_role.name
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_func.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/GET/get-todo"
+}
