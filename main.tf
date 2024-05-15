9346a58f3babb72b99d90520795db6b47a61ae0e
EC2 Default User <ec2-user@ip-172-31-46-98.us-west-2.compute.internal>
2024-05-15 17:28:32 +0000

    Fix AWS login and clean up main.tf

--- main.tf
+++ main.tf
@@ -17,19 +17,6 @@ resource "aws_ecr_repository" "my_first_ecr_repo" {
   name = "c2-g4-tf-ecr-repo" # Naming my repository
 }
 
-resource "aws_s3_bucket" "to_do_list_bucket" {
-	
-	bucket 	= "c2-g4-tf-us-west-2-962804699607"
-
-}
-
-resource "aws_s3_bucket_object" "object" {
-
-  bucket = aws_s3_bucket.to_do_list_bucket.id
-  key    = "todo-data.json"
-  source = "todo-data.json"
-
-}
 
 #CREATING IAM ROLES
 
@@ -73,8 +60,30 @@ resource "aws_iam_role" "codebuild_role" {
   
 }
 
-# CREATE CUSTOM POLICIES
+resource "aws_iam_role" "codepipeline_role" {
+  
+  name   = "c2-g4-tf-codepipeline-role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "codepipeline.amazonaws.com"
+                ]
+            }
+        }
+    ]
+  })
+  
+}
 
+# CREATE CUSTOM POLICIES
+# this could porbably be replaced by a standard AWS lambda policy or role?
 resource "aws_iam_policy" "iam_policy_for_lambda" {
  
  name         = "c2-g4-tf-iam-lambda-policy"
@@ -129,12 +138,34 @@ resource "aws_iam_role_policy_attachment" "attach_policy_to_codebuild_role" {
   role        = aws_iam_role.codebuild_role.name
 }
  
+# CREATE S3 BUCKET FOR TODO-DATA.JSON
+# create the bucket itself
+resource "aws_s3_bucket" "to_do_list_bucket" {
+	
+	bucket 	= "c2-g4-tf-us-west-2-962804699607"
+
+}
+
+# CREATE S3 BUCKET FOR TODO-DATA.JSON
+# create an object in the bucket, which will be our json file
+resource "aws_s3_bucket_object" "object" {
+
+  bucket = aws_s3_bucket.to_do_list_bucket.id
+  key    = "todo-data.json"
+  source = "todo-data.json"
+
+}
+
+# CREATE THE LAMBDA
+# first zip the lambda_function code
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
   source_dir  = "${path.module}/python/"
   output_path = "${path.module}/python/lambda_function.zip"
 }
- 
+
+#CREATE THE LAMBDA
+# then define the lambda itself using our zip file defined above
 resource "aws_lambda_function" "terraform_lambda_func" {
   filename                       = "${path.module}/python/lambda_function.zip"
   function_name                  = "c2-g4-tf-get-to-do"
@@ -144,6 +175,8 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
+#CREATE THE API GATEWAY
+# the basics
 resource "aws_api_gateway_rest_api" "my_api" {
 
   name = "c2-g4-tf-todo-api"
@@ -155,12 +188,16 @@ resource "aws_api_gateway_rest_api" "my_api" {
   
 }
 
+#CREATE THE API GATEWAY
+# define the path to the endpoint
 resource "aws_api_gateway_resource" "root" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   parent_id = aws_api_gateway_rest_api.my_api.root_resource_id
   path_part = "get-todo"
 }
 
+#CREATE THE API GATEWAY
+# define a method for our resource/path defined above to be hit by our users
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   resource_id = aws_api_gateway_resource.root.id
@@ -168,6 +205,11 @@ resource "aws_api_gateway_method" "proxy" {
   authorization = "NONE"
 }
 
+#CREATE THE API GATEWAY
+# define the handoff from the gateway to the lambda
+# - note the integration_http_method below is POST though we receive a GET above
+# - the gateway METHOD above can accept any http_method and we're receiving a GET from the user
+# - but the lambda wants to receive a POST in the handoff from the gateway to the lambda
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   resource_id = aws_api_gateway_resource.root.id
@@ -177,14 +219,10 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri = aws_lambda_function.terraform_lambda_func.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
 
-}
-
+#CREATE THE API GATEWAY
+# define the response from the lambda back to the gateway
+# - note the response_templates definition, which was required before we could get this API fully working
 resource "aws_api_gateway_integration_response" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   resource_id = aws_api_gateway_resource.root.id
@@ -201,51 +239,30 @@ resource "aws_api_gateway_integration_response" "proxy" {
   ]
 }
 
-resource "aws_api_gateway_method" "proxy_options" {
+#CREATE THE API GATEWAY
+# define the response from the gateway back to the user
+resource "aws_api_gateway_method_response" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   resource_id = aws_api_gateway_resource.root.id
-  http_method = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_integration_options" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy_options.http_method
-  integration_http_method = "POST"
-  type = "MOCK"
-}
-
-resource "aws_api_gateway_method_response" "proxy_options" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy_options.http_method
+  http_method = aws_api_gateway_method.proxy.http_method
   status_code = "200"
+
 }
 
-resource "aws_api_gateway_integration_response" "proxy_options" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy_options.http_method
-  status_code = aws_api_gateway_method_response.proxy_options.status_code
-
-  depends_on = [
-    aws_api_gateway_method.proxy_options,
-    aws_api_gateway_integration.lambda_integration_options
-  ]
-}
-
+#CREATE THE API GATEWAY
+# define the stage for the gateway
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
-    aws_api_gateway_integration.lambda_integration_options, # Add this line
+    #aws_api_gateway_integration.lambda_integration_options, # Add this line if we return the OPTIONS method back to our resource/path
   ]
 
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name = "prod"
 }
 
-
+#CREATE THE API GATEWAY
+# connect our gateway with our lambda
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id = "AllowExecutionFromAPIGateway"
   action = "lambda:InvokeFunction"
